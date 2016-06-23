b2df82a12cef9325220d9ba68714432f40c00d5a
qiuqi <qiu.qiq@gmail.com>
2016-06-23 23:59:07 +0800

    fix load wrong path

--- src/salt_nif.erl
+++ src/salt_nif.erl
@@ -48,18 +48,8 @@
 %%%
 
 load() ->
-    SoName = case code:priv_dir(?APPNAME) of
-                 {error, bad_name} ->
-                     case filelib:is_dir(filename:join(["..", priv])) of
-                         true ->
-                             filename:join(["..", priv, ?LIBNAME]);
-                         _ ->
-                             filename:join([priv, ?LIBNAME])
-                     end;
-                 Dir ->
-                     filename:join(Dir, ?LIBNAME)
-             end,
-    erlang:load_nif(SoName, 0).
+    Path = filename:join([code:priv_dir(salt),"salt_nif"]),
+    erlang:load_nif(Path, 0).
 
 %%% Exported from salt_nif.c.
 
